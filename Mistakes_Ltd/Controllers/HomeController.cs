ee711bab7806ea702dc6139c0e4b4dbb7292a69a
EllieRuston <88114202+EllieRuston@users.noreply.github.com>
2023-03-20 15:03:47 +1300

    Add project files.

---
+++ Mistakes_Ltd/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mistakes_Ltd.Models;
+using System.Diagnostics;
+
+namespace Mistakes_Ltd.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ Mistakes_Ltd/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Mistakes_Ltd.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
